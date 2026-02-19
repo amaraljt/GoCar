ceb3a01c4536f3b0cb03327f031fe5b82fa286c9
Jared Amaral <jaredamaral1229@gmail.com>
2026-02-18 21:17:41 -0800

    read adc value from potentiometer(incorrect values)

--- GoCar_Controller/Core/Src/main.c
+++ GoCar_Controller/Core/Src/main.c
@@ -19,12 +19,17 @@
 /* Private variables ---------------------------------------------------------*/
 UART_HandleTypeDef huart1;
 I2C_HandleTypeDef hi2c1;
+ADC_HandleTypeDef hadc1;
+
+uint16_t g_raw_angle = 0;
+uint32_t g_adc_val = 0;
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2C1_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_ADC1_Init(void);
 //static int __io_putchar(int ch);
 
 /**
@@ -33,25 +38,47 @@ static void MX_USART1_UART_Init(void);
   */
 int main(void)
 {
+  char buffer[20];
+  char buffer1[20];
+
   SystemClock_Config();
 
   MX_GPIO_Init();
   MX_I2C1_Init();
   MX_USART1_UART_Init();
+  MX_ADC1_Init();
 
   lcd_init(&hi2c1);
   lcd_clear();
 
   while (1)
   {
-    uint16_t g_raw_angle = read_raw_angle(&hi2c1);
+    /* Read raw angle */
+    g_raw_angle = read_raw_angle(&hi2c1);
 
-    char buffer[20];
-    sprintf(buffer, "RAW ANGLE : %4d", g_raw_angle);
+    HAL_ADC_Start(&hadc1);
+    if(HAL_OK != HAL_ADC_PollForConversion(&hadc1, HAL_MAX_DELAY)){
+      lcd_clear();
+      lcd_put_cur(0, 0);
+      lcd_send_string("ADC ERROR");
+      return 1;
+    }
+    
+    /* Read potentiometer value */
+    g_adc_val = HAL_ADC_GetValue(&hadc1);
+    HAL_ADC_Stop(&hadc1);
+
+    /* PRINT ANGLE */
+    sprintf(buffer, "ANGLE: %4d", g_raw_angle);
     lcd_put_cur(0, 0);
     lcd_send_string(buffer);
     HAL_Delay(200);
-    //lcd_clear();  
+
+    /* PRINT ADC */
+    sprintf(buffer1, "ADC: %4d", g_adc_val);
+    lcd_put_cur(1, 0);
+    lcd_send_string(buffer1);
+    HAL_Delay(200);
   }
 }
 
@@ -180,6 +207,58 @@ static void MX_GPIO_Init(void)
   /* USER CODE BEGIN MX_GPIO_Init_2 */
 
   /* USER CODE END MX_GPIO_Init_2 */
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV2;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = DISABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time.
+  */
+  sConfig.Channel = ADC_CHANNEL_1;
+  sConfig.Rank = 1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_3CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 // static int __io_putchar(int ch)
